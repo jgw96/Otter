e1b1edb47af37372b221d0dfd3a2cccb01d50c2e
jgw96 <jgw9617@gmail.com>
2023-01-01 16:25:27 -0800

    fix

--- src/components/search.ts
+++ src/components/search.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css, PropertyValueMap } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 
 import '@shoelace-style/shoelace/dist/components/input/input.js';
