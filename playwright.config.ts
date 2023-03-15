d8feb6fbed59e9cd2284ac9697c4a860f267f273
jgw96 <jgw9617@gmail.com>
2023-03-14 21:48:15 -0700

    more tests, testing is now working nicely

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -3,15 +3,14 @@ import { test, expect } from '@playwright/test';
 const url = 'https://wonderful-glacier-07b022d1e.2.azurestaticapps.net/';
 
 // get username and password from environment variables
-// const username = process.env.MAS_USERNAME || "";
-// const password = process.env.MAS_PASSWORD || "";
+const username = process.env.MAS_USERNAME || "";
+const password = process.env.MAS_PASSWORD || "";
 
 // before each test
 test.beforeEach(async ({ page }) => {
     await page.goto(url);
 });
 
-// Test suite
 test('ensure application loads', async ({ page }) => {
     // find the button with the text "Login"
     const loginButton = page.locator('text=Login');
@@ -20,34 +19,48 @@ test('ensure application loads', async ({ page }) => {
     await expect(loginButton).toBeVisible();
 });
 
-// test('ensure login works', async ({ page }) => {
-//     await page.getByLabel('').click();
-//     await page.getByLabel('').fill('tech.lgbt');
-//     await page.getByRole('button', { name: 'Login' }).click();
-//     await page.waitForLoadState('networkidle');
-//     await page.getByLabel('E-mail address').fill(username);
-//     await page.getByLabel('Password').click();
-//     await page.getByLabel('Password').fill(password);
-//     await page.getByLabel('Password').press('Enter');
-//     await page.getByRole('button', { name: 'Authorize' }).click();
-//     await page.waitForLoadState('networkidle');
+test('ensure home page loads with server and token', async ({ page }) => {
+    // add token to local storage
+    await page.evaluate(() => {
+        localStorage.setItem("server", "https://tech.lgbt");
+        localStorage.setItem("accessToken", "eXQJuum5bGmy5xkB_Xzx19_FThAmhI0W-dbBQp671CU");
+    });
 
-//     // url should be ${url}home after login
-//     expect(page.url()).toBe(`${url}home`);
-// });
+    await page.reload();
 
-// test('ensure login works with https', async ({ page }) => {
-//     await page.getByLabel('').click();
-//     await page.getByLabel('').fill('https://tech.lgbt');
-//     await page.getByRole('button', { name: 'Login' }).click();
-//     await page.waitForLoadState('networkidle');
-//     await page.getByLabel('E-mail address').fill(username);
-//     await page.getByLabel('Password').click();
-//     await page.getByLabel('Password').fill(password);
-//     await page.getByLabel('Password').press('Enter');
-//     await page.getByRole('button', { name: 'Authorize' }).click();
-//     await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('networkidle');
 
-//     // url should be ${url}home after login
-//     expect(page.url()).toBe(`${url}home`);
-// })
+    // ensure the url contains /home
+    expect(page.url()).toContain('/home');
+});
+
+test('ensure timeline loads on home page', async ({ page }) => {
+    await page.evaluate(() => {
+        localStorage.setItem("server", "https://tech.lgbt");
+        localStorage.setItem("accessToken", "eXQJuum5bGmy5xkB_Xzx19_FThAmhI0W-dbBQp671CU");
+    });
+
+    await page.reload();
+
+    await page.waitForLoadState('networkidle');
+
+    // expect the timeline to be visible
+    await expect(page.locator('#sl-tab-panel-1 app-timeline')).toBeVisible();
+});
+
+test('ensure that you can switch tabs, such as to the notifications tab', async ({ page }) => {
+    await page.evaluate(() => {
+        localStorage.setItem("server", "https://tech.lgbt");
+        localStorage.setItem("accessToken", "eXQJuum5bGmy5xkB_Xzx19_FThAmhI0W-dbBQp671CU");
+    });
+
+    await page.reload();
+
+    await page.waitForLoadState('networkidle');
+
+    // click the notifications tab
+    await page.click('sl-tab[panel="notifications"]');
+
+    // expect the notifications tab to be visible
+    await expect(page.locator('#sl-tab-panel-7 app-notifications')).toBeVisible();
+});
