e53f91dd4d2b6d4612b090c890558e89ade12314
jgw96 <jgw9617@gmail.com>
2023-04-13 22:18:19 -0700

    fix build issue

--- src/pages/app-explore.ts
+++ src/pages/app-explore.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from 'lit';
-import { customElement, property, state } from 'lit/decorators.js'
+import { customElement, state } from 'lit/decorators.js'
 import { getPreviewTimeline } from '../services/timeline';
 
 import '../components/preview-timeline';
